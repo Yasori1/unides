7607bb2fc2fe84026ff4bf46ae2e31de5eecfc17
exxsy <yasar451278@gmail.com>
2025-11-20 15:59:42 +0300

    First fire bug-fixes
    Style implementations, angular configurations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { RouterOutlet, Router, Event, NavigationEnd } from '@angular/router';
 
 @Component({
     selector: 'app-root',
+    standalone: true,
     imports: [RouterOutlet],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
