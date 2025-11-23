fef6d51e16a93962268dd438f8204a846c0510d8
Safa Rahman Cenkci <safacenkci@gmail.com>
2025-11-23 18:23:20 +0300

    Ana Sayfa Güncellemeleri

--- src/app/pages/home/banner/banner.component.ts
+++ src/app/pages/home/banner/banner.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
-    selector: 'app-banner',
-    imports: [RouterLink],
-    templateUrl: './banner.component.html',
-    styleUrl: './banner.component.scss'
+  selector: 'app-banner',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './banner.component.html',
+  styleUrl: './banner.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA], // Spline viewer etiketini tanimasi icin gerekli
 })
-export class BannerComponent {}
+export class BannerComponent implements OnInit {
+  ngOnInit(): void {
+    // Spline Viewer scriptini dinamik olarak yukluyoruz
+    // Bu islem React'teki import Spline mantiginin Angular karsiligidir.
+    const script = document.createElement('script');
+    script.type = 'module';
+    script.src = 'https://unpkg.com/@splinetool/viewer@1.9.59/build/spline-viewer.js';
+    document.head.appendChild(script);
+  }
+}
