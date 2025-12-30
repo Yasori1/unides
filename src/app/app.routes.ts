e66887a3c5d4e0831e252746db70f09ac6de6156
karoxyyy <abdullahgonc2@gmail.com>
2025-12-30 20:09:40 +0300

    updates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,36 +1,14 @@
 import { Routes } from '@angular/router';
 import { ErrorPageComponent } from './pages/error-page/error-page.component';
 import { HomeComponent } from './pages/home/home.component';
-import { Home3Component } from './pages/home3/home3.component'; // YENİ EKLENDİ
-import { HomeDemoTwoComponent } from './demos/home-demo-two/home-demo-two.component';
-import { HomeDemoThreeComponent } from './demos/home-demo-three/home-demo-three.component';
-import { HomeDemoFourComponent } from './demos/home-demo-four/home-demo-four.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { TeamPageComponent } from './pages/team-page/team-page.component';
 import { ContactPageComponent } from './pages/contact-page/contact-page.component';
-import { HowItWorksPageComponent } from './pages/how-it-works-page/how-it-works-page.component';
 import { FaqPageComponent } from './pages/faq-page/faq-page.component';
-import { ShopPageComponent } from './pages/shop-page/shop-page.component';
-import { CartPageComponent } from './pages/cart-page/cart-page.component';
-import { CheckoutPageComponent } from './pages/checkout-page/checkout-page.component';
-import { ProductDetailsPageComponent } from './pages/product-details-page/product-details-page.component';
-import { BlogGridPageComponent } from './pages/blog-grid-page/blog-grid-page.component';
-import { BlogRightSidebarPageComponent } from './pages/blog-right-sidebar-page/blog-right-sidebar-page.component';
-import { BlogLeftSidebarPageComponent } from './pages/blog-left-sidebar-page/blog-left-sidebar-page.component';
-import { BlogDetailsPageComponent } from './pages/blog-details-page/blog-details-page.component';
-import { BlogDetailsPage2Component } from './pages/blog-details-page2/blog-details-page2.component';
-import { BlogDetailsPage3Component } from './pages/blog-details-page3/blog-details-page3.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
-import { CategoriesPageComponent } from './pages/categories-page/categories-page.component';
 import { PrivacyPolicyPageComponent } from './pages/privacy-policy-page/privacy-policy-page.component';
 import { TermsConditionsPageComponent } from './pages/terms-conditions-page/terms-conditions-page.component';
-import { ListingsPage1Component } from './pages/listings-page1/listings-page1.component';
-import { ListingsPage2Component } from './pages/listings-page2/listings-page2.component';
-import { ListingsPage3Component } from './pages/listings-page3/listings-page3.component';
-import { ListingsPage4Component } from './pages/listings-page4/listings-page4.component';
-import { ListingsPage5Component } from './pages/listings-page5/listings-page5.component';
-import { ListingDetailsComponent } from './pages/listing-details/listing-details.component';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { DashboardComponent } from './admin-dashboard/dashboard/dashboard.component';
 import { MyListingsComponent } from './admin-dashboard/my-listings/my-listings.component';
@@ -63,9 +41,6 @@ import { CommunityDashboardComponent } from './pages/community-dashboard/communi
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-
-  // YENİ EKLENEN V3 ROTASI
-  { path: 'v3', component: Home3Component },
 
   // Ana Sayfalar
   { path: 'developers', component: DevelopersPageComponent },
