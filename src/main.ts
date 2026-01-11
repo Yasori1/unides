d5505bd6ad099f7af79bb987dc8dbe106db8afec
Safa Rahman Cenkci <safacenkci@gmail.com>
2026-01-11 13:51:38 +0300

    güncellemeler

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { registerLocaleData } from '@angular/common';
+import localeTr from '@angular/common/locales/tr';
 import 'zone.js';
+
+// Türkçe locale verisini yükle
+registerLocaleData(localeTr);
 
 bootstrapApplication(AppComponent, appConfig)
     .catch((err) => console.error(err));
