1bc52d0b9b3055f41839f14b9f4d662dc8d32c64
Safa Rahman Cenkci <safacenkci@gmail.com>
2026-03-05 00:42:47 +0300

    Güncelleme

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -106,11 +106,13 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   // --- Geçmişten Kareler Slider ---
   sliderImages: string[] = [
-    'assets/gecmisten-kareler/gecmisten-kareler-1.png',
-    'assets/gecmisten-kareler/gecmisten-kareler-2.png',
-    'assets/gecmisten-kareler/gecmisten-kareler-3.png',
-    'assets/gecmisten-kareler/gecmisten-kareler-4.png',
-    'assets/gecmisten-kareler/gecmisten-kareler-5.png',
+    '/assets/gecmisten-kareler/gecmisten-kareler-1.png',
+    '/assets/gecmisten-kareler/gecmisten-kareler-2.png',
+    '/assets/gecmisten-kareler/gecmisten-kareler-3.png',
+    '/assets/gecmisten-kareler/gecmisten-kareler-4.png',
+    '/assets/gecmisten-kareler/gecmisten-kareler-5.png',
+    '/assets/gecmisten-kareler/gecmisten-kareler-6.png',
+    '/assets/gecmisten-kareler/gecmisten-kareler-7.png?v=1',
   ];
 
   // --- Lightbox ---
